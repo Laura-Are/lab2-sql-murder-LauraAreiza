fef6d3ba5e97a201e1b6d9596b53cad2b5aa8787
Laura-Are <lauraareizahenao@gmail.com>
2026-03-08 21:07:18 -0500

    Actualizacion del caso. Paso 5 y 6

--- consultas/respuestas.sql
+++ consultas/respuestas.sql
@@ -10,3 +10,31 @@ WHERE date = "20180115" AND city = "SQL City";
 Select * from person
 WHERE name LIKE ("%Annabel%");
 
+-- 3. Busque la entrevista que se le hizo a la segunda testigo. Se descubrio que la testigo 
+-- reconocio al asesino el dia 9 de enero cuando entrenaba en el gimnasio.
+Select * from interview
+	Join person
+		On interview.person_id = person.id
+Where id = "16371";
+
+-- 4. Busque las personas que entrenaron en el gimnasio el dia 9 de enero. suponemos que es del 
+-- 2018. Descubrimos que 10 personas entraron ese dia al gimnasio.
+Select * from get_fit_now_check_in
+where check_in_date LIKE ("%20180109%");
+
+-- 5. Ahora buscare por el lado del segundo testigo, para obtener mas información, sabemos que vive
+-- en la última casa de Northwestern Dr. Descubrimos que se llama Morty Schapiro, y tiene el id
+-- 14887
+Select * from person
+WHERE address_street_name LIKE ("%Northwestern%")
+ORDER BY address_number DESC
+LIMIT 5;
+
+-- 6. Busque la entrevista que le hicieron al primer testigo. Se descubrio que su numero de 
+-- membresia gold del gimnasio empieza por "48z". Ademas, se subió a un coche con 
+-- matrícula "H42W". 
+Select * from interview
+	Join person
+		On interview.person_id = person.id
+Where id = "14887";
+
